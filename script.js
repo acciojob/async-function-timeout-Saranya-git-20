dc0b617ab0c1034ff5b15f74230101db16502451
Saranya Barani <62207142+Saranya-git-20@users.noreply.github.com>
2025-07-30 22:15:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+document.getElementById("btn").addEventListener("click", async function () {
+	await setTextDelayValue();
+});
+
+function delay(ms, res) {
+	return new Promise((resolve) => {
+		setTimeout(() => resolve(res), ms);
+	});
+}
+
+async function setTextDelayValue() {
+	const text = document.getElementById('text').value;
+	const delayTime = parseInt(document.getElementById('delay').value);
+	const resultBody = document.getElementById('output');
+
+	const result = await delay(delayTime, text);
+	resultBody.textContent = `result - ${delayTime}`;
+}
